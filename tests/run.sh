5b31d6cebba20f2c8a924dd3deeb9a5d78678b89
caua <cauaalvarenganeves@gmail.com>
2026-06-23 10:37:53 -0300

    collate: UCA (DUCET) with full CollationTest 16.0 conformance
    - tools/gen.cst: DUCET table from allkeys.txt (single CEs + 964 contractions,
      CE pool) + Unified_Ideograph table for implicit weights
    - collate/collate.cst: NFD -> collation elements with contiguous AND
      discontiguous (S2.1.1) contractions + implicit weights (Han FB40/FB80,
      Tangut/Nushu/Khitan, unassigned FBC0); sort_key with NON_IGNORABLE and
      SHIFTED variable weighting; compare
    - conf_collate: CollationTest NON_IGNORABLE (206325) + SHIFTED (227840) pass

--- tests/run.sh
+++ tests/run.sh
@@ -46,6 +46,7 @@ run_case conf_sentence  tests/conformance/conf_sentence.cst
 run_case conf_linebreak tests/conformance/conf_linebreak.cst
 run_case conf_bidi      tests/conformance/conf_bidi.cst
 run_case test_idna      tests/test_idna.cst
+run_case conf_collate   tests/conformance/conf_collate.cst
 
 if [ "$fail" -eq 0 ]; then echo "all green"; else echo "FAILURES"; fi
 exit $fail
