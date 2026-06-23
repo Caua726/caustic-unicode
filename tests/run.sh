1ae460bc3aecd6178ce4b1aab07f4cd767a9b4ba
caua <cauaalvarenganeves@gmail.com>
2026-06-23 03:10:05 -0300

    case: lower/upper/title + folding with tr/az/lt tailoring
    - tools/gen.cst: caseprops bitset (Cased/Case_Ignorable/Soft_Dotted),
      simplecase (lower/upper/title triples), fold (simple+full), special
      casing (sorted by cp, condition codes); write_trie helper
    - case/case.cst: simple+full mappings, SpecialCasing context conditions
      (Final_Sigma, More_Above, After_Soft_Dotted, Before_Dot, After_I) +
      locale tr/az/lt; full case folding; eq_fold; titlecasing; _u8 wrappers
    - props: is_cased/is_case_ignorable/is_soft_dotted
    - test_case: 17 checks (ß->SS, ligatures, final sigma, Turkish I, folding)

--- tests/run.sh
+++ tests/run.sh
@@ -39,6 +39,7 @@ run_case test_utf       tests/test_utf.cst
 run_case test_width     tests/test_width.cst
 run_case test_normalize tests/test_normalize.cst
 run_case conf_normalize tests/conformance/conf_normalize.cst
+run_case test_case      tests/test_case.cst
 
 if [ "$fail" -eq 0 ]; then echo "all green"; else echo "FAILURES"; fi
 exit $fail
